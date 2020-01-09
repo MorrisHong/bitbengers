72c1c71dc7e1eaa20b581464a90ae6bbc187d0bf
gracelove <govlmo91@gmail.com>
2020-01-09 13:33:33 +0900

    head = null

--- src/main/java/minho/linkedList/MyLinkedListImpl.java
+++ src/main/java/minho/linkedList/MyLinkedListImpl.java
@@ -8,7 +8,7 @@ public class MyLinkedListImpl<T> implements MyLinkedList<T> {
     private int size;
 
     public MyLinkedListImpl() {
-        head = new Node<>();
+        head = null;
         size = 0;
     }
 
